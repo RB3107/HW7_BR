43f6b8f265cbbdf4db26bc9b7bcc5b2e7e5295dc
Raisa01~ <zarvirova@mail.ru>
2022-12-04 00:15:31 +0300

    Task 52

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ void PrintArray(double[,] array)
 
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.Write("Введите строку: ");
+/*Console.Write("Введите строку: ");
 int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
 Console.Write("Введите столбец: ");
 int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
@@ -60,5 +60,31 @@ Console.WriteLine("Элемент не существует  ");
     else
 {
     Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("---------------------------");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
 }
 Console.ReadLine();
