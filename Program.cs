ebc39a15615b8454b0528722649308890db8fb58
Raisa01~ <zarvirova@mail.ru>
2022-12-04 00:11:35 +0300

    Task 50

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
 
-Console.WriteLine("Введите количество строк  ");
+/*Console.WriteLine("Введите количество строк  ");
 int linesVol = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов");
 int columnsVol = Convert.ToInt32(Console.ReadLine());
@@ -31,4 +31,34 @@ void PrintArray(double[,] array)
         Console.Write("]");
         Console.WriteLine("");
     }
+}*/
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Console.Write("Введите строку: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5; 
+int m = 7; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
 }
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
